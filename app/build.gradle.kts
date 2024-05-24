ff24e72e9e9debf8763945ca16e0708a93468ac7
superXnova21 <107920527+superXnova21@users.noreply.github.com>
2024-05-24 23:55:30 +0600

    espresso dependencies(modified) added and AdminPanel is modified and rules are added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,8 @@ dependencies {
     // Robolectric dependencies for unit tests
     testImplementation("org.robolectric:robolectric:4.7.3")  //to run Android tests in the JVM without the need for an emulator or real device.
 
+    //dependencies for ui_test
+    androidTestImplementation("androidx.test:rules:1.5.0")
 
 
 }
