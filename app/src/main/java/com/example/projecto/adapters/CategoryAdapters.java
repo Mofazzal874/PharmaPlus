b8ad8e5c2c80a4d2f0385602ec473b9a2799d432
superXnova21 <107920527+superXnova21@users.noreply.github.com>
2024-05-27 12:18:51 +0600

    Modified all the adapters(category, suggested, view all), Added javadocs and some code refinements

--- app/src/main/java/com/example/projecto/adapters/CategoryAdapters.java
+++ app/src/main/java/com/example/projecto/adapters/CategoryAdapters.java
@@ -1,6 +1,5 @@
 package com.example.projecto.adapters;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
@@ -19,47 +18,72 @@ import com.example.projecto.models.CategoryModel;
 
 import java.util.List;
 
+/**
+ * Adapter class for displaying categories in a RecyclerView.
+ * This adapter manages the binding of CategoryModel data into the UI elements within a RecyclerView.
+ */
 public class CategoryAdapters extends RecyclerView.Adapter<CategoryAdapters.ViewHolder> {
 
-    Context context;
-    List<CategoryModel> categoryModelList;
+    private Context context;
+    private List<CategoryModel> categoryModelList;
 
+    /**
+     * Constructor for CategoryAdapters.
+     * @param context The current context.
+     * @param categoryModelList The list of categories to be displayed.
+     */
     public CategoryAdapters(Context context, List<CategoryModel> categoryModelList) {
         this.context = context;
         this.categoryModelList = categoryModelList;
     }
 
+    /**
+     * Inflates the view for each category item.
+     * @param parent The ViewGroup into which the new View will be added after it is bound to an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new ViewHolder that holds the View for each category item.
+     */
     @NonNull
     @Override
-    public CategoryAdapters.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.category_item,parent,false));
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_item, parent, false);
+        return new ViewHolder(itemView);
     }
 
+    /**
+     * Binds data to the view holder at the specified position in the RecyclerView.
+     * @param holder The ViewHolder which should be updated to represent the contents of the item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
-    public void onBindViewHolder(@NonNull CategoryAdapters.ViewHolder holder, int position) {
-        Glide.with(context).load(categoryModelList.get(position).getImg_url()).into(holder.catImg);
-        holder.name.setText(categoryModelList.get(position).getName());
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        CategoryModel model = categoryModelList.get(position);
+        Glide.with(context).load(model.getImg_url()).into(holder.catImg);
+        holder.name.setText(model.getName());
 
-
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, ViewAllActivity.class);
-                intent.putExtra("type",categoryModelList.get(holder.getAdapterPosition()).getType());
-                context.startActivity(intent);
-            }
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, ViewAllActivity.class);
+            intent.putExtra("type", model.getType());
+            context.startActivity(intent);
         });
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     * @return The total number of items in this adapter.
+     */
     @Override
     public int getItemCount() {
         return categoryModelList.size();
     }
 
+    /**
+     * Provides a reference to the type of views that you are using (custom ViewHolder).
+     */
     public class ViewHolder extends RecyclerView.ViewHolder {
-
         ImageView catImg;
         TextView name;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             catImg = itemView.findViewById(R.id.cat_img);
